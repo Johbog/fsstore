e6fe6678d62b47cc04f61bcc9ac0971a4a1db78b
Johan <johan@webdesq.net>
2023-03-22 21:41:20 +0100

    async calls & prepared schemas

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { randomUUID } = require('crypto');
 
 class Store {
   constructor(name, data, index, dir) {
-    console.log(dir);
     this._name = name;
     this._data = data || [];
     this._index = index || {};
@@ -14,35 +13,49 @@ class Store {
   }
 
   get(id) {
-    const i = this._index[id];
-    if (i > -1) {
-      return this._data[i];
-    }
+    return new Promise(resolve => {
+      const i = this._index[id];
+      if (i > -1) {
+        resolve(this._data[i]);
+      }
+    });
   }
 
   getAll() {
-    return this._data;
+    return new Promise(resolve => {
+      resolve(this._data);
+    });
   }
 
   async create(data) {
-    const _data = Object.assign({}, data);
-    if (!_data.id) {
-      _data.id = randomUUID();
-    }
-    this._data.push(_data);
-    this._index[_data.id] = this._data.length - 1;
+    const id = await new Promise(resolve => {
+      const _data = Object.assign({}, data);
+      if (!_data.id) {
+        _data.id = randomUUID();
+      }
+      this._data.push(_data);
+      this._index[_data.id] = this._data.length - 1;
+      resolve(_data.id);
+    });
 
-    await this.save();
+    this.save();
+
+    return { created: [ id ] };
   }
 
   async set(id, data) {
-    const i = this._index[id];
-    if (i < 0) {
-      throw `Could not find item with id "${id}"`;
-    }
-    this._data[i] = Object.assign({}, data);
+    await new Promise(resolve => {
+      const i = this._index[id];
+      if (i < 0) {
+        throw `Could not find item with id "${id}"`;
+      }
+      this._data[i] = Object.assign({}, data);
+      resolve();
+    });
 
-    await this.save();
+    this.save();
+
+    return { updated: [ id ] };
   }
 
   async save() {
@@ -50,15 +63,21 @@ class Store {
     const indexFile = resolve(this.dir, 'index');
 
     await mkdir(this.dir, { recursive: true });
+    // TODO: cleanup file
     await Promise.all([
       writeFile(dataFile, this._data.map(r => JSON.stringify(r)).join('\n')),
       writeFile(indexFile, JSON.stringify(this._index)),
     ]);
   }
+
+  rebuildIndex() {
+    // TODO:
+  }
 }
 
 class Storage {
-  constructor(name) {
+  constructor(name, options) {
+    this._options = Object.assign({ schemas: {} }, options);
     this._dir = resolve(__dirname, '.dbs', name);
     this._stores = {};
   }
@@ -102,27 +121,48 @@ class Storage {
     }
 
     return this._stores[name];
-    // this._stores[storeName];
   }
 }
 
 (async() => {
   try {
-    const db = new Storage('test');
+    const schemas = {
+      contacts: {
+        indices: [
+          {
+            name: 'fullname',
+            keyPath: [ 'name.first', 'name.middle', 'name.last' ],
+            unique: false,
+            multiEntry: false,
+            locale: null, // TODO
+          },
+        ],
+        properties: {
+          name: {
+            type: 'object',
+            properties: {
+              first: {
+                type: 'string',
+                required: true,
+              },
+              middle: {
+                type: 'string',
+              },
+              last: {
+                type: 'string',
+                required: true,
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const db = new Storage('test', { schemas });
     await db.load();
 
     const result = await db.store('contacts').get('2b1cc7d3-6be5-442a-81e5-537d0c5d7606');
     console.log(result);
-
-    /* await db.store('contacts').create({
-      name: {
-        first: 'Piet',
-        middle: '',
-        last: 'Pietsma',
-      },
-    }); */
-
-
 
     process.exit();
   }
